73fa113b423cff721cfa7ade9a9f2e292c7e927a
Jaehyun Yoon <yuchem2@gmail.com>
2025-02-13 12:43:41 +0900

    feat: add basic button (#10)

---
+++ src/components/button.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components'
+
+import { ChevronDownIcon } from './icon'
+import { ButtonTypes } from '../types/button.ts'
+
+interface Props {
+    text: string
+    type: ButtonTypes
+    onClick: () => void
+}
+
+const palette = [
+    {
+        color: '#292929',
+        background: '#FFFFFF',
+        stroke: '#D9D9D9',
+    },
+    {
+        color: '#FAFCFE',
+        background: '#35376E',
+        stroke: '#292959',
+    },
+]
+
+function Button({ text, type, onClick }: Props) {
+    return (
+        <ButtonLayout color={palette[type].color} background={palette[type].background} stroke={palette[type].stroke} onClick={onClick}>
+            <ButtonText>{text}</ButtonText>
+            <ChevronDownIcon width={'1.2rem'} height={'0.9rem'} fill={palette[type].color} />
+        </ButtonLayout>
+    )
+}
+
+const ButtonLayout = styled.div<{ color: string, background: string, stroke: string }>`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 0.1rem solid ${props => props.stroke};
+    border-radius: 50rem;
+    background-color: ${props => props.background};
+    color: ${props => props.color};
+    width: fit-content;
+    column-gap: 0.4rem;
+    padding: 0.8rem 1.2rem;
+`
+
+const ButtonText = styled.span`
+    font-size: 1.4rem;
+    text-align: center;
+`
+
+export default Button
